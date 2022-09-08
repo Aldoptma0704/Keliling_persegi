b0c9c8763b60be44445218b96eff0d3c0cf9ad35
Aldo Pratama <112595321+Aldoptma0704@users.noreply.github.com>
2022-09-08 09:49:24 +0700

    Add files via upload

---
+++ keliling persegi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int s,keliling;
+
+    cout<<"masukan sisi persegi : ";
+    cin>>s;
+
+    keliling=4*s;
+    cout<<"luas persegi adalah "<<keliling;
+    return 0 ;
+}
